5dde8a471dc7f29390d9c3ab1ebe78c3ed72b557
Andezion <245122@edu.p.lodz.pl>
2025-03-14 23:34:12 +0100

    начал добавлять списки и скоро походу нужна будет база ебаная

--- src/main/java/com/example/lera/MainView.java
+++ src/main/java/com/example/lera/MainView.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -23,24 +24,83 @@ public class MainView extends Application
     {
         HBox root = new HBox();
 
-        VBox leftPane = createPane("Категории", Color.LIGHTGRAY, 300);
+        VBox leftPane = createPane("Categories", Color.LIGHTGRAY, 300);
         leftPane.setAlignment(Pos.TOP_CENTER);
 
         VBox centerPane = createPane("Список запчастей", Color.WHITE, 1000);
         centerPane.setAlignment(Pos.TOP_LEFT);
 
-        VBox rightPane = createPane("Описание детали", Color.LIGHTBLUE, 100);
+        VBox rightPane = createPane("Options", Color.LIGHTBLUE, 100);
         rightPane.setAlignment(Pos.TOP_CENTER);
         rightPane.setSpacing(30);
 
         root.getChildren().addAll(leftPane, centerPane, rightPane);
 
-        Button user_button = new Button("User");
-        Button tran_button = new Button("Transaction");
+        Button user_button = new Button("Users");
+        Button tran_button = new Button("Buy");
         Button order_button = new Button("Order");
         Button parts_button = new Button("Parts");
 
         rightPane.getChildren().addAll(user_button, tran_button, order_button, parts_button);
+
+        ComboBox<String> firstBox = new ComboBox<>();
+        ComboBox<String> secondBox = new ComboBox<>();
+        ComboBox<String> thirdBox = new ComboBox<>();
+
+        firstBox.getItems().addAll("Звёзды", "Цепи", "Фильтры");
+        firstBox.setPromptText("Выберите категорию");
+
+        secondBox.setDisable(true);
+        thirdBox.setDisable(true);
+
+        leftPane.getChildren().addAll(firstBox, secondBox, thirdBox);
+
+        firstBox.setOnAction(event ->
+        {
+            secondBox.getItems().clear();
+            secondBox.setDisable(false);
+            thirdBox.setDisable(true);
+
+            String selected = firstBox.getValue();
+            if ("Звёзды".equals(selected))
+            {
+                secondBox.getItems().addAll("Поршни", "Коленвал", "Фильтр");
+            }
+            else if ("Цепи".equals(selected))
+            {
+                secondBox.getItems().addAll("Амортизаторы", "Рычаги", "Пружины");
+            }
+            else if ("Фильтры".equals(selected))
+            {
+                secondBox.getItems().addAll("Аккумулятор", "Стартер", "Фара");
+            }
+            secondBox.setPromptText("Выберите подкатегорию");
+        });
+
+        secondBox.setOnAction(event ->
+        {
+            thirdBox.getItems().clear();
+            thirdBox.setDisable(false);
+
+            String selected = secondBox.getValue();
+            if ("Поршни".equals(selected))
+            {
+                thirdBox.getItems().addAll("Поршень A", "Поршень B");
+            }
+            else if ("Коленвал".equals(selected))
+            {
+                thirdBox.getItems().addAll("Коленвал X", "Коленвал Y");
+            }
+            else if ("Амортизаторы".equals(selected))
+            {
+                thirdBox.getItems().addAll("Передний", "Задний");
+            }
+            else if ("Фара".equals(selected))
+            {
+                thirdBox.getItems().addAll("Фара LED", "Галоген");
+            }
+            thirdBox.setPromptText("Выберите деталь");
+        });
 
         Scene scene = new Scene(root, 1400, 700);
 
