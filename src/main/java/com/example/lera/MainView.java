31f4cb52e77fb37909721d8f0543317f71fa39e1
Andezion <245122@edu.p.lodz.pl>
2025-03-25 22:03:36 +0100

    user and orders is ready

--- src/main/java/com/example/lera/MainView.java
+++ src/main/java/com/example/lera/MainView.java
@@ -2,13 +2,12 @@ package com.example.lera;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -21,7 +20,7 @@ import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -56,6 +55,8 @@ public class MainView extends Application
 
         rightPane.getChildren().addAll(user_button, tran_button, order_button, parts_button);
 
+        user_button.setOnAction(e -> loadUsers());
+        order_button.setOnAction(event -> loadDatabase());
         parts_button.setOnAction(e -> {
 
             if (!leftPane.getChildren().isEmpty() && pointer_for_button == 0)
@@ -155,6 +156,195 @@ public class MainView extends Application
         stage.setTitle("Test");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void loadUsers()
+    {
+        centerPane.getChildren().clear();
+
+        String URL = "jdbc:postgresql://localhost:5432/postgres";
+        String USER = "postgres";
+        String PASSWORD = "Ffdss321!";
+
+        // Новый запрос для выборки данных клиентов и заказов
+        String query = "SELECT c.id AS client_id, c.first_name, c.last_name, " +
+                "o.id AS order_id, o.order_date, o.total_amount " +
+                "FROM clients c " +
+                "JOIN client_orders co ON c.id = co.client_id " +
+                "JOIN orders o ON co.order_id = o.id " +
+                "ORDER BY o.order_date DESC;"; // Сортировка по дате заказа (от новых к старым)
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query))
+        {
+
+            GridPane grid = new GridPane();
+            grid.setPadding(new Insets(10));
+            grid.setHgap(1);
+            grid.setVgap(1);
+            grid.setAlignment(Pos.CENTER);
+            grid.setStyle("-fx-grid-lines-visible: true; -fx-border-color: black; -fx-border-width: 1px;");
+
+            String[] headers = {"ID", "First Name", "Last Name", "Order ID", "Order Date", "Total Amount"};
+            for (int col = 0; col < headers.length; col++)
+            {
+                Label headerLabel = new Label(headers[col]);
+                headerLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold; -fx-border-color: black; -fx-border-width: 1px; -fx-padding: 5px;");
+                headerLabel.setAlignment(Pos.CENTER);
+                headerLabel.setMaxWidth(Double.MAX_VALUE);
+                GridPane.setHalignment(headerLabel, HPos.CENTER);
+                grid.add(headerLabel, col, 0);
+            }
+
+            int row = 1;
+            while (rs.next())
+            {
+                int clientId = rs.getInt("client_id");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                int orderId = rs.getInt("order_id");
+                String orderDate = rs.getString("order_date");
+                String totalAmount = rs.getString("total_amount");
+
+                Label clientIdLabel = new Label(String.valueOf(clientId));
+                Label firstNameLabel = new Label(firstName);
+                Label lastNameLabel = new Label(lastName);
+                Label orderIdLabel = new Label(String.valueOf(orderId));
+                Label orderDateLabel = new Label(orderDate);
+                Label totalAmountLabel = new Label(totalAmount);
+
+                clientIdLabel.setAlignment(Pos.CENTER);
+                firstNameLabel.setAlignment(Pos.CENTER);
+                lastNameLabel.setAlignment(Pos.CENTER);
+                orderIdLabel.setAlignment(Pos.CENTER);
+                orderDateLabel.setAlignment(Pos.CENTER);
+                totalAmountLabel.setAlignment(Pos.CENTER_LEFT);
+
+                grid.add(clientIdLabel, 0, row);
+                grid.add(firstNameLabel, 1, row);
+                grid.add(lastNameLabel, 2, row);
+                grid.add(orderIdLabel, 3, row);
+                grid.add(orderDateLabel, 4, row);
+                grid.add(totalAmountLabel, 5, row);
+
+                row++;
+            }
+
+            ColumnConstraints col1 = new ColumnConstraints(80);
+            ColumnConstraints col2 = new ColumnConstraints(150, 200, Double.MAX_VALUE);
+            ColumnConstraints col3 = new ColumnConstraints(150, 200, Double.MAX_VALUE);
+            ColumnConstraints col4 = new ColumnConstraints(100, 150, Double.MAX_VALUE);
+            ColumnConstraints col5 = new ColumnConstraints(150, 200, Double.MAX_VALUE);
+            ColumnConstraints col6 = new ColumnConstraints(150, 200, Double.MAX_VALUE);
+
+            col2.setHalignment(HPos.CENTER);
+            col3.setHalignment(HPos.CENTER);
+            col4.setHalignment(HPos.CENTER);
+            col5.setHalignment(HPos.CENTER);
+            col6.setHalignment(HPos.CENTER);
+
+            grid.getColumnConstraints().addAll(col1, col2, col3, col4, col5, col6);
+
+            ScrollPane scrollPane = new ScrollPane(grid);
+            scrollPane.setFitToWidth(true);
+            scrollPane.setPrefHeight(centerPane.getHeight());
+
+            centerPane.getChildren().add(scrollPane);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadDatabase()
+    {
+        centerPane.getChildren().clear();
+
+        String URL = "jdbc:postgresql://localhost:5432/postgres";
+        String USER = "postgres";
+        String PASSWORD = "Ffdss321!";
+
+        String query = "SELECT o.id, o.order_date, o.total_amount, " +
+                "STRING_AGG(p.name, ', ') AS parts_list " +
+                "FROM orders o " +
+                "JOIN order_parts op ON o.id = op.order_id " +
+                "JOIN warehouse w ON op.warehouse_id = w.id " +
+                "JOIN parts p ON w.part_id = p.id " +
+                "GROUP BY o.id, o.order_date, o.total_amount " +
+                "ORDER BY o.id;";
+
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query))
+        {
+
+            GridPane grid = new GridPane();
+            grid.setPadding(new Insets(10));
+            grid.setHgap(1);
+            grid.setVgap(1);
+            grid.setAlignment(Pos.CENTER);
+            grid.setStyle("-fx-grid-lines-visible: true; -fx-border-color: black; -fx-border-width: 1px;");
+
+            String[] headers = {"ID", "Date of Order", "Sum of Order", "Details of Order"};
+            for (int col = 0; col < headers.length; col++)
+            {
+                Label headerLabel = new Label(headers[col]);
+                headerLabel.setStyle("-fx-font-size: 18px; -fx-font-weight: bold; -fx-border-color: black; -fx-border-width: 1px; -fx-padding: 5px;");
+                headerLabel.setAlignment(Pos.CENTER);
+                headerLabel.setMaxWidth(Double.MAX_VALUE);
+                GridPane.setHalignment(headerLabel, HPos.CENTER);
+                grid.add(headerLabel, col, 0);
+            }
+
+            int row = 1;
+            while (rs.next())
+            {
+                int orderId = rs.getInt("id");
+                String orderDate = rs.getString("order_date");
+                String totalAmount = rs.getString("total_amount");
+                String partsList = rs.getString("parts_list");
+
+                Label orderIdLabel = new Label(String.valueOf(orderId));
+                Label orderDateLabel = new Label(orderDate);
+                Label totalAmountLabel = new Label(totalAmount);
+                Label partsListLabel = new Label(partsList);
+
+                orderIdLabel.setAlignment(Pos.CENTER);
+                orderDateLabel.setAlignment(Pos.CENTER);
+                totalAmountLabel.setAlignment(Pos.CENTER_LEFT);
+                partsListLabel.setAlignment(Pos.CENTER_LEFT);
+
+                grid.add(orderIdLabel, 0, row);
+                grid.add(orderDateLabel, 1, row);
+                grid.add(totalAmountLabel, 2, row);
+                grid.add(partsListLabel, 3, row);
+
+                row++;
+            }
+
+            ColumnConstraints col1 = new ColumnConstraints(80);
+            ColumnConstraints col2 = new ColumnConstraints(150, 200, Double.MAX_VALUE);
+            ColumnConstraints col3 = new ColumnConstraints(150, 200, Double.MAX_VALUE);
+            ColumnConstraints col4 = new ColumnConstraints(300, 500, Double.MAX_VALUE);
+
+            col2.setHalignment(HPos.CENTER);
+            col3.setHalignment(HPos.CENTER);
+            col4.setHgrow(Priority.ALWAYS);
+
+            grid.getColumnConstraints().addAll(col1, col2, col3, col4);
+
+            ScrollPane scrollPane = new ScrollPane(grid);
+            scrollPane.setFitToWidth(true);
+            scrollPane.setPrefHeight(centerPane.getHeight());
+
+            centerPane.getChildren().add(scrollPane);
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     private void updateCenterPane(Part partDetails)
